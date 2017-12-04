4d75c144ef5fefc4dad7310404c017398f298fa7
Ruben Bert Pingol <rubenberttpingol@gmail.com>
2017-12-04 16:05:42 +0800

    v1.0.1 with form input controls serialization with toggling of verbose or not verbose type

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Simple form utility library.
 This form utility library aims to provide developers a time-efficient way of handling common form events, like:
 * Form Validation
 * Form Submission
-* Grid Row Form *(within a Table)*
+* Table Grid Row *(within a Table)*
 
 But having still the full-control of what happens next after an event happened.
 
@@ -15,3 +15,10 @@ But having still the full-control of what happens next after an event happened.
 ### Optional Dependencies
 * Select2 *(If you like to render `<select>` tag differently from the default **Bootstrap** theme)*
 * jQuery *(For you to load the **Select2**)*
+
+### Change Log
+#### v1.0.0
+* Initial form-utility setup files and dependencies
+
+### v1.0.1
+* Added form input controls serialization with toggling of verbose and not verbose
--- js/form-utility.js
+++ js/form-utility.js
@@ -1,29 +1,223 @@
 (function(){
-    this.FormUtility = function() {
-	var defaults = {};
+    this.FormUtility = function FormUtility() {
 
-	if (arguments[0] && typeof arguments[0] === "object") {
-	    this.options = extendedDefaults(defaults, arguments[0]);
-	}
+        this.formSelector = null;
+        this.myForm = null;
 
-	initializeMethods.call(this);
+        var defaults = {
+            select2: false
+        };
+
+        if (arguments && arguments.length !== 0) {
+
+            // Check length of arguments if more than two, throw an error
+            if (arguments.length > 2) throw new Error("FormUtility library only accepts one or two parameters. first: \"the form selector (e.g. id, class, name)\", second: \"FormUtility options or configurations\"");
+
+            var argInd;
+            for (argInd in arguments) {
+                switch (typeof arguments[argInd]) {
+                    case "string":
+                        if (parseInt(argInd) === 0) {
+                            this.formSelector = arguments[argInd];
+                        }
+                        break;
+
+                    case "object":
+                        if (parseInt(argInd) === 1) {
+                            this.options = _extendedDefaults(defaults, arguments[argInd]);
+                        }
+                        break;
+                        
+                    default:
+                        break;
+                }
+            }
+        }
+
+        _initializeMethods.call(this);
     };
 
-    FormUtility.prototype.init = function() {
-	console.log(this);
+    FormUtility.prototype.init = function init() {
+        var _this = this;
+
+        // Catch if no formSelector defined
+        if (_this.formSelector === null) throw new ReferenceError("FormUtility Error: No form selector was supplied to FormUtility instance. Please define one.");
+
+        _this.formSelector = _this.formSelector.replace(/\s/g, "");
+
+        if (document.getElementById(_this.formSelector)) { // Test if formSelector is an ID
+            _this.myForm = document.getElementById(_this.formSelector);
+        } else if (document.forms[_this.formSelector]) { // Test if formSelector is a form name attribute
+            _this.myForm = document.forms[_this.formSelector];
+        } else { // Test if formSelector is a CSS class
+            _this.myForm = document.getElementsByClassName(_this.formSelector).length > 0 ? document.getElementsByClassName(_this.formSelector)[0] : _this.myForm;
+        }
+
+        var privateMyForm = _this.myForm;
+
+        // Check if privateMyForm is null, then throw an error
+        if (privateMyForm === null) throw new DOMException("FormUtility cannot find the specified form from the given form selector: "+_this.formSelector, "UndefinedForm");
+
+        HTMLFormElement.prototype.getElements = function() {
+            return _getFormElements.call(_this);
+        }
+        HTMLFormElement.prototype.serialize = function(arg) {
+            arg = (arg===undefined) ? false : arg;
+
+            return _serializeForm.call(_this, arg);
+        }
+
     };
 
-    function extendedDefaults(source, properties) {
-	var property;
-	for (property in properties) {
-	    if (properties.hasOwnProperty(property)) {
-		source[property] = properties[property];
-	    }
-	}
-	return source;
+    FormUtility.prototype.getForm = function getForm() {
+        var _this = this;
+
+        if (!_this.myForm) throw new DOMException("FormUtility cannot find the specified form from the given form selector: "+_this.formSelector, "UndefinedForm");
+
+        return _this.myForm;
+    };
+
+    FormUtility.prototype.getFormElements = function formElements() {
+        var _this = this;
+
+        if (_this.myForm===null) throw new DOMException("FormUtility cannot find the specified form from the given form selector: "+_this.formSelector, "UndefinedForm");
+
+        return _getFormElements.call(_this);
+    };
+
+    FormUtility.prototype.formSerialize = function formSerialize() {
+        var arg = (arguments[0] && arguments[0].length > 0) ? arguments[0] : false;
+            
+        return _serializeForm.call(this, arg);
+    };
+
+    FormUtility.prototype.formSubmit = function formSubmit() {
+        var _this = this;
+        var _form = _this.myForm;
+        var _args = arguments;
+        var _defaults = {};
+
+        if (arguments[0] && typeof arguments[0] === "object") {
+            // Check for "callback" property on arguments[0]
+            if (arguments[0].hasOwnProperty("callback") && typeof arguments[0].callback === "function") {
+                _defaults = _extendedDefaults(_defaults, arguments[0]);
+            }
+        }
+
+        if (window.addEventListener) {
+            _form.addEventListener("submit", function(event) {
+                event.preventDefault();
+                
+                if (_args.callee.name === "formSubmit") {
+                    _defaults.callback(_this.myForm, event);
+                }
+            });
+        }
+    };
+
+    FormUtility.prototype.tableGrid = function tableGrid() {
+
+    };
+
+    function _getFormElements() {
+
+        var elements = (this.myForm === undefined) ? this.elements : this.myForm.elements;
+        var filteredElements = [];
+        
+        if (elements) {
+            for (var elem in elements) {
+                if (!isNaN(parseInt(elem))) {
+                    switch (elements[elem].type) {
+                        case 'text':
+                        case 'email':
+                        case 'date':
+                        case 'number':
+                        case 'password':
+                        case 'tel':
+                        case 'radio':
+                        case 'checkbox':
+                        case 'select-one':
+                        case 'textarea':
+                            filteredElements.push(elements[elem]);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        return filteredElements;
     }
 
-    function initializeMethods() {
-	this.init.bind(this);
+    /**
+     * Method to serialize form elements into server-readable inputs
+     * 
+     * @param boolean $verbose Flag if detailed FormData or not
+     * 
+     * @return instanceof FormData $data
+     */
+    function _serializeForm($verbose) {
+        var elements = _getFormElements.call(this);
+        var serializedElements = {};
+
+        if (elements) {
+            for (var elem in elements) {
+                if (!isNaN(parseInt(elem))) {
+                    switch (elements[elem].type) {
+                        case 'radio':
+                            if (elements[elem].checked) {
+                                
+                                if ($verbose) {
+                                    var data = {
+                                        type: elements[elem].type,
+                                        value: elements[elem].value
+                                    };
+
+                                    serializedElements[elements[elem].name] = data;
+                                    continue;
+                                }
+
+                                serializedElements[elements[elem].name] = elements[elem].value;
+                            }
+                            break;
+                        default:
+                            if ($verbose) {
+                                var data = {
+                                    type: elements[elem].type,
+                                    value: elements[elem].value
+                                };
+
+                                serializedElements[elements[elem].name] = data;
+                                continue;
+                            }
+
+                            serializedElements[elements[elem].name] = elements[elem].value;
+                            break;
+                    }
+                }
+            }
+            return JSON.stringify(serializedElements);
+        }
+    }
+
+    function _extendedDefaults(source, properties) {
+        var property;
+        for (property in properties) {
+            if (properties.hasOwnProperty(property)) {
+                source[property] = properties[property];
+            }
+        }
+        return source;
+    }
+
+    function _initializeMethods() {
+        this.init.bind(this);
+        this.getForm.bind(this);
+        this.getFormElements.bind(this);
+        this.formSerialize.bind(this);
+        this.formSubmit.bind(this);
+        this.tableGrid.bind(this);
     }
 }());
